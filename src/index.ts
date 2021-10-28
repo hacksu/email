1c5a5f8db732c6a0e772946760f80070c6141187
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-10-28 12:01:52 -0400

    exported sendgrid

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export const MATCH_SUBSTITUTION_TAGS = /(%\w+%)/g;
 
 export * from './email';
 export * from './styles';
+export * from './sendgrid';
 import { Email, EmailTemplate } from './email';
 
 // compute all the templates
